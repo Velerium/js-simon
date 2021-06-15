258f0b44019d3465000b62d866c3cef5525f0bcb
Velerium <bondaniandrea@libero.it>
2021-06-15 16:20:43 +0200

    Added more distractions. Fixed **critical** mistake. You could just input the same correct answer five times and you'd win...

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener ('DOMContentLoaded', function() {
 
     var output = document.querySelector('#output-numbers');
+    var start = document.querySelector('#let-you-know');
+    var start2 = document.querySelector('.start');
     var userNum = document.querySelector('.user-numbers');
     var cpuNum = document.querySelector('.cpu-numbers');
     var comment = document.querySelector('.comment');
@@ -9,6 +11,10 @@ document.addEventListener ('DOMContentLoaded', function() {
     var difficulty = document.querySelectorAll('.difficulty');
     var distraction = document.querySelectorAll('.distraction')[0];
     var distraction2 = document.querySelectorAll('.distraction')[1];
+    var distraction3 = document.querySelectorAll('.distraction')[2];
+    var distraction4 = document.querySelectorAll('.distraction')[3];
+    var distraction5 = document.querySelectorAll('.distraction')[4];
+    var distraction6 = document.querySelectorAll('.distraction')[5];
     var time = document.querySelector('#time');
     var timer = document.querySelector('.timer')
 
@@ -33,26 +39,49 @@ document.addEventListener ('DOMContentLoaded', function() {
     }
 
     function countdown() {
-        distraction.style.display = 'block';
-        distraction2.style.display = 'block';
-        time.innerHTML = s + 's';
-        var timing = setInterval (function() {
-            s--;
+        delay();
+        setTimeout (function() {
+            distraction.style.display = 'inline-block';
+            distraction2.style.display = 'inline-block';
+            distraction3.style.display = 'inline-block';
+            distraction4.style.display = 'inline-block';
+            distraction5.style.display = 'inline-block';
+            distraction6.style.display = 'inline-block';
             time.innerHTML = s + 's';
-            decoy();
-            if (s === 0) {
-                clearInterval(timing);
-                game();
-            }
-        }, 1000)
+            var timing = setInterval (function() {
+                s--;
+                time.innerHTML = s + 's';
+                decoy();
+                if (s === 0) {
+                    clearInterval(timing);
+                    game();
+                }
+            }, 1000)
+        }, 3000)
+    }
+
+    function delay () {
+        start2.style.display = 'block';
+        setTimeout (function() {
+            start2.style.display = 'none';
+            timer.style.display = 'block';
+        }, 3000)
     }
 
     function decoy() {
         var bait = setInterval(function() {
             var random = Math.floor(Math.random()*999);
             var random2 = Math.floor(Math.random()*999);
+            var random3 = Math.floor(Math.random()*999);
+            var random4 = Math.floor(Math.random()*999);
+            var random5 = Math.floor(Math.random()*999);
+            var random6 = Math.floor(Math.random()*999);
             distraction.innerHTML = random;
             distraction2.innerHTML = random2;
+            distraction3.innerHTML = random3;
+            distraction4.innerHTML = random4;
+            distraction5.innerHTML = random5;
+            distraction6.innerHTML = random6;
             if (s === 0) {
                 clearInterval(bait);
             }
@@ -64,18 +93,27 @@ document.addEventListener ('DOMContentLoaded', function() {
         timer.style.display = 'none';
         distraction.style.display = 'none';
         distraction2.style.display = 'none';
+        distraction3.style.display = 'none';
+        distraction4.style.display = 'none';
+        distraction5.style.display = 'none';
+        distraction6.style.display = 'none';
         userNum.style.display = 'block';
         cpuNum.style.display = 'block';
 
         for(n = 0; n < 5; n++) {
 
             do {
+                var flag = false;
                 var guess = parseInt(prompt('Insert the numbers you remember from before!'));
                 if(isNaN(guess)) {
                     alert('Invalid value!');
+                    flag = true
+                } else if (userNumbers.includes(guess)) {
+                    alert("Fair enough, you tried. You already guessed that one!")
+                    flag = true;
                 }
             }
-            while(isNaN(guess))
+            while(flag === true)
 
             if (cpuNumbers.includes(guess)) {
                 userNumbers.push(guess);
@@ -127,7 +165,7 @@ document.addEventListener ('DOMContentLoaded', function() {
         numGen();
         cpuNumbers.sort(function(a,b) {return a-b});
         if (generated === 1) {
-            alert(cpuNumbers);
+            start.innerHTML = cpuNumbers;
         }
 
         countdown();
